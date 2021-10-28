de063e0a77ccc570da7037bf41f4c7cced12eb0b
abnerlucss <abner.santos@bandtec.com.br>
2021-10-28 17:04:42 -0300

    Alterando a query que retorna a quantidade de voos e embarques previstos para hoje

--- src/main/java/com/example/abnerlucss/msvembarque/repository/EmbarqueRepository.java
+++ src/main/java/com/example/abnerlucss/msvembarque/repository/EmbarqueRepository.java
@@ -11,6 +11,6 @@ import java.util.Optional;
 public interface EmbarqueRepository extends JpaRepository<Embarque, Integer> {
     Optional<Embarque> findByCodigoLocalizador(String codigoLocalizador);
 
-    @Query(value = "select count(id_embarque) from embarque e where data_hora_embarque::date = current_date;", nativeQuery = true)
+    @Query(value = "select count(id_embarque) from embarque e where date(data_hora_embarque) = current_date;", nativeQuery = true)
     Integer embarquePrevistosParaHoje();
 }
